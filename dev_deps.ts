c78cb4d8e8465f53c0777763e7da0a4ec9eee054
Yichuan Shen <shen.yichuan@gmail.com>
2021-08-31 23:25:50 +0200

    Update dependencies

--- dev_deps.ts
+++ dev_deps.ts
@@ -5,18 +5,18 @@ export {
   assert,
   assertEquals,
   assertThrows,
-} from "https://deno.land/std@0.85.0/testing/asserts.ts";
+} from "https://deno.land/std@0.106.0/testing/asserts.ts";
 
-export { delay } from "https://deno.land/std@0.85.0/async/mod.ts";
+export { delay } from "https://deno.land/std@0.106.0/async/mod.ts";
 
 export {
   copy,
   ensureDir,
   move,
   walk as walkDir,
-} from "https://deno.land/std@0.85.0/fs/mod.ts";
+} from "https://deno.land/std@0.106.0/fs/mod.ts";
 
 export {
   fromFileUrl as pathFromFileUrl,
   resolve as resolvePath,
-} from "https://deno.land/std@0.85.0/path/mod.ts";
+} from "https://deno.land/std@0.106.0/path/mod.ts";
