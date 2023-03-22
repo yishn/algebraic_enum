99290fbc27e5c39bdf6b6516d5bc041a253285fd
Yichuan Shen <shen.yichuan@gmail.com>
2023-03-22 02:40:32 +0100

    Update deno std lib

--- dev_deps.ts
+++ dev_deps.ts
@@ -1,22 +1,22 @@
-export { expectType } from "https://cdn.skypack.dev/ts-expect@1.1.0?dts";
-export type { TypeEqual } from "https://cdn.skypack.dev/ts-expect@1.1.0?dts";
+export { expectType } from "npm:ts-expect@1.1.0";
+export type { TypeEqual } from "npm:ts-expect@1.1.0";
 
 export {
   assert,
   assertEquals,
   assertThrows,
-} from "https://deno.land/std@0.106.0/testing/asserts.ts";
+} from "https://deno.land/std@0.180.0/testing/asserts.ts";
 
-export { delay } from "https://deno.land/std@0.106.0/async/mod.ts";
+export { delay } from "https://deno.land/std@0.180.0/async/mod.ts";
 
 export {
   copy,
   ensureDir,
   move,
   walk as walkDir,
-} from "https://deno.land/std@0.106.0/fs/mod.ts";
+} from "https://deno.land/std@0.180.0/fs/mod.ts";
 
 export {
   fromFileUrl as pathFromFileUrl,
   resolve as resolvePath,
-} from "https://deno.land/std@0.106.0/path/mod.ts";
+} from "https://deno.land/std@0.180.0/path/mod.ts";
